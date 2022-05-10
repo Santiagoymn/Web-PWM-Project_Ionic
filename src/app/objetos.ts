59925510b69563d162f8e5fb08530566dbe3027f
SamuelRodri <samuelrh2001@hotmail.com>
2022-05-10 09:52:15 +0100

    SPRINT 4: Añadido objetos y ficheros de login añadidos

---
+++ src/app/objetos.ts
@@ -0,0 +1,70 @@
+export interface Categoria {
+  titulo: string;
+  descripcion: string;
+  url: string;
+  alt: string;
+  actividades: Actividad[];
+}
+export interface Actividad {
+  name: string;
+  url: string;
+  description: string;
+  empresas: Empresa[];
+}
+export interface Empresa {
+  name: string;
+  place: string;
+  price: number;
+  tNumber: string;
+  web: string;
+}
+export interface SobreNosotrosGeneral{
+  logo: string;
+  alt: string;
+  descripcion: string;
+}
+export interface Carrusel {
+  url: string;
+  alt: string;
+}
+export interface Persona {
+  foto: string;
+  alt: string;
+  descripcion: string;
+}
+export interface Galeria {
+  url: string;
+  descripcion: string;
+}
+export interface DiscoverGC {
+  municipio: string;
+  descripcion: string;
+  urlImagen: string;
+  imagenDropdown: string;
+}
+export interface Evento{
+  nombre: string;
+  fecha: string;
+  lugar: string;
+  municipio: string;
+  url: string;
+  descripcion: string;
+  dia: string;
+  mes: string;
+}
+export interface Usuario{
+  usuario: string;
+  email: string;
+  nombre: string;
+  apellidos: string;
+  password: string;
+  id: number;
+}
+
+export interface UsuarioFire{
+  usuario: string;
+  nombre: string;
+  apellidos: string;
+}
+
+
