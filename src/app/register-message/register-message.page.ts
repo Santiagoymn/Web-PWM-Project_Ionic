982e6f72c90e4d3d66a2413d75256f0adf385cef
meelbaperez <merima001@hotmail.com>
2022-05-16 23:55:10 +0100

    Registro y mensaje funcionalidad terminada

---
+++ src/app/register-message/register-message.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-message',
+  templateUrl: './register-message.page.html',
+  styleUrls: ['./register-message.page.scss'],
+})
+export class RegisterMessagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
